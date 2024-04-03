3d8ea8468050a3d7ea093c0846ec9f0b834e2d1f
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-04-03 22:09:04 +0530

    Create pgm1.sh

---
+++ exp2/pgm1.sh
@@ -0,0 +1,15 @@
+echo "Enter your Name"
+read name
+echo "Enter you Semester"
+read semester
+echo "Enter batch"
+read batch
+echo "Enter Rollno"
+read Rollno
+echo "Enter Register no"
+echo "Semester :"$semester
+read registerno
+echo "Name :"$name
+echo "Batch :"$batch
+echo "Roll no :"$Rollno
+echo "Register no :"$registerno
