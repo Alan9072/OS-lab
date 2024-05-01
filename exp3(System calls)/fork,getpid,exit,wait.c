3e7d4166aae6b199093c8ec78ec96bbba1817b63
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-05-01 17:10:05 +0530

    Update fork,getpid,exit,wait.c

--- exp3(System calls)/fork,getpid,exit,wait.c
+++ exp3(System calls)/fork,getpid,exit,wait.c
@@ -1,1 +1,35 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdlib.h>
 
+int main()
+{
+
+	
+	pid_t childpid=fork();
+	if(childpid==-1)
+	{
+		printf("child execution unsuccessful\n");
+		
+	}
+	else if(childpid==0){
+		printf("child process\n");
+		printf("pid %d \n",getpid());
+		printf("Parent pid %d \n",getppid());
+		
+		
+	}
+	else{
+		printf("parent process \n");
+		printf("PID %d\n",getpid());
+		printf("child PID %d\n",childpid);
+		wait(NULL);
+		printf("child finished");
+		exit(0);
+	}
+	return 0;
+	
+	
+}
